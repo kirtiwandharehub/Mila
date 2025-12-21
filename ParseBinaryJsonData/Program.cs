3abc4171acbe41e819003f1369b27caf5dc81261
kirti wandhare <kirtiwandhare@outlook.com>
2025-12-20 17:13:11 -0800

    add first commit

---
+++ ParseBinaryJsonData/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+byte[] jsonBinaryData = File.ReadAllBytes(@"SchoolStaff.json");
+//Console.WriteLine(jsonBinaryData.Length);
+string jsonStringData = Encoding.UTF8.GetString(jsonBinaryData);
+//Console.WriteLine(jsonStringData);
+SchoolStaff[] schoolStaff = JsonConvert.DeserializeObject<SchoolStaff[]>(jsonStringData);
+Console.WriteLine(schoolStaff.Length);
+foreach(SchoolStaff ss in schoolStaff)
+{
+    Console.WriteLine($"FirstName: {ss.FirstName} - LastName: {ss.LastName} - Grade: {ss.Grade}");
+}
---
+++ ParseBinaryJsonData/SchoolStaff.cs
@@ -0,0 +1,7 @@
+
+public class SchoolStaff
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Grade { get; set; }
+}
